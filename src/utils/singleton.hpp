1578ea8f4df4042a1b75b596ff24eb9419b503d0
Christian <logicismmc@gmail.com>
2024-02-22 16:53:13 +0800

    Create `singleton` utility

---
+++ src/utils/singleton.hpp
@@ -0,0 +1,22 @@
+namespace narechi::utils
+{
+    template<typename T>
+    class singleton
+    {
+    public:
+        static T& get()
+        {
+            static T instance;
+            return instance;
+        }
+
+        // Remove copy operators
+        void operator=(const singleton&) = delete;
+        singleton(const singleton&) = delete;
+
+    protected:
+        singleton() {}
+        virtual ~singleton() {}
+
+    };
+}
