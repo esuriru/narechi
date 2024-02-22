9086d7fd1b81f6ea5c72ed7f14cd058b1c37a684
Christian <logicismmc@gmail.com>
2024-02-22 11:39:48 +0800

    Add `NRC_ASSERT` assert macro

---
+++ src/core/assert.hpp
@@ -0,0 +1,13 @@
+#include <core/logger.hpp>
+
+#ifdef NRC_DEBUG
+    #define NRC_ASSERT(cond, ...)\
+    if (!(cond))\
+    {\
+        NRC_CORE_ERROR("Assertion failed: ", __VA_ARGS__,\
+            "\n\tat line number: ", __LINE__, "\n\tin file: ",\
+            __FILE__);\
+    }
+#else
+    #define NRC_ASSERT(cond, ...)
+#endif
--- src/entry.cpp
+++ src/entry.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+#include "core/core.hpp"
 #include "core/logger.hpp"
+#include "core/assert.hpp"
 
 int main()
 {
     narechi::core_logger.log(
         narechi::logger::log_level::info, "Test ", "Bam");
+    NRC_ASSERT(false, "Test assertion");
 }
