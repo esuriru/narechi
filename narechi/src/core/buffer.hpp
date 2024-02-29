be007d48e28f02704ff378b5c46163a6d457e9be
Christian <97773844+esuriru@users.noreply.github.com>
2024-03-01 01:25:34 +0800

    Create base data buffer

---
+++ narechi/src/core/buffer.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+#include <string.h>
+
+namespace narechi
+{
+    struct buffer
+    {
+        uint8_t* data = nullptr;
+        uint64_t size = 0;
+
+        buffer() = default;
+        buffer(const buffer&) = default;
+
+        buffer(uint64_t size)
+        {
+
+        }
+
+        static buffer clone(const buffer& other) 
+        {
+            buffer temp(other.size);
+            memcpy(temp.data, other.data, other.size);
+            return temp;
+        }
+
+        void allocate(uint64_t size)
+        {
+            release();
+
+            data = new uint8_t[size];            
+            this->size = size;
+        }
+
+        void release()
+        {
+            delete[] data;
+
+            // Reset values to default
+            data = nullptr;
+            size = 0;
+        }
+
+        operator bool() const
+        {
+            return (bool)data;
+        }
+    };
+}
