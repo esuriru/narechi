5ebb73ba3d585b84f014a8f5d94a0506d204c3d6
Christian <97773844+esuriru@users.noreply.github.com>
2024-02-29 23:18:43 +0800

    Add `std::shared_ptr` type alias

--- narechi/src/core/core.hpp
+++ narechi/src/core/core.hpp
@@ -8,3 +8,17 @@
 #else
     #error "narechi only runs on windows"
 #endif
+
+#include <memory>
+
+namespace narechi
+{
+    template<typename T>
+    using sptr = std::shared_ptr<T>;
+
+    template<typename T, typename... Args>
+    constexpr sptr<T> create_shared(Args&&... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+}
