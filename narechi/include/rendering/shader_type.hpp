d5fe10a630bdb9255815426fca9fe28970443e19
Christian <97773844+esuriru@users.noreply.github.com>
2024-08-09 14:16:21 +0800

    Replace false assert with fatal log

--- narechi/include/rendering/shader_type.hpp
+++ narechi/include/rendering/shader_type.hpp
@@ -52,8 +52,8 @@ namespace narechi
             break;
         }
 
-        NRC_ASSERT(
-            false, "shader data type not handled, shader data type: ", type);
+        NRC_CORE_FATAL(
+            "shader data type not handled, shader data type: ", type);
         return 0;
     }
 }
