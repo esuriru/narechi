110296c9bd4b25c671d9a254c5df6ae66f7967a5
Christian <97773844+esuriru@users.noreply.github.com>
2024-08-11 16:37:43 +0800

    Add renderer API based graphics context create factory method

--- narechi/include/rendering/graphics_context.hpp
+++ narechi/include/rendering/graphics_context.hpp
@@ -4,9 +4,15 @@
 
 namespace narechi
 {
+    class app;
+
     class graphics_context
     {
     public:
+        virtual ~graphics_context();
+
+        static uptr<graphics_context> create(app* app);
+
         virtual void init() = 0;
         virtual void swap_buffers() = 0;
     };
