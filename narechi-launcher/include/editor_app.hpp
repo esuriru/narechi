b7d3946108a721c486961ce2b262369f4af0b509
Christian <97773844+esuriru@users.noreply.github.com>
2024-09-06 19:24:21 +0800

    Remove TODO

--- narechi-launcher/include/editor_app.hpp
+++ narechi-launcher/include/editor_app.hpp
@@ -3,9 +3,6 @@
 #include "editor_layer.hpp"
 #include "narechi.hpp"
 
-// TODO:
-// https://www.youtube.com/watch?v=88dmtleVywk&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=11
-
 namespace narechi::editor
 {
     class editor_app : public app
