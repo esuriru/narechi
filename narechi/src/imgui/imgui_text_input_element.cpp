d4711cb015c48337c5976765fd57b344505a94dd
Christian <97773844+esuriru@users.noreply.github.com>
2024-09-18 20:11:15 +0800

    Add `imgui_text_input_element` render impl

---
+++ narechi/src/imgui/imgui_text_input_element.cpp
@@ -0,0 +1,18 @@
+#include "imgui/imgui_text_input_element.hpp"
+
+#include "imgui.h" // IWYU pragma: keep (IMGUI_API forward decl)
+#include "misc/cpp/imgui_stdlib.h"
+
+namespace narechi
+{
+    imgui_text_input_element::imgui_text_input_element(
+        const gui::text_input_element_properties& props)
+        : text_input_element(props)
+    {
+    }
+
+    void imgui_text_input_element::render()
+    {
+        ImGui::InputText(props.label.c_str(), &props.text, 0);
+    }
+}
