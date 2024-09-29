048b2a114abdb004e74ea90ebaeec1c300b25caa
Christian <97773844+esuriru@users.noreply.github.com>
2024-09-29 23:17:42 +0800

    Create `window_flags` style flags

--- narechi/include/gui/window.hpp
+++ narechi/include/gui/window.hpp
@@ -2,11 +2,20 @@
 
 #include "core/core.hpp"
 #include "gui/element.hpp"
+#include "gui/text_input_element.hpp"
+#include "utils/enum_flag_utils.hpp"
 
 #include <string>
 
 namespace narechi::gui
 {
+    enum class window_flags
+    {
+        align_text_elements = 1 << 0,
+    };
+
+    ENUM_FLAG_OPERATORS(window_flags);
+
     struct window_properties
     {
         std::string name;
@@ -14,6 +23,7 @@ namespace narechi::gui
         uint32_t width = 0, height = 0;
 
         uint32_t start_position_x = 0, start_position_y = 0;
+        window_flags flags {};
 
         static bool verify(const window_properties& props);
     };
@@ -27,6 +37,7 @@ namespace narechi::gui
 
         // TODO - Maybe a base check on element class? Implement NVI
         virtual void add_element(sptr<element> element) = 0;
+        virtual void add_element(sptr<text_input_element> element) = 0;
 
         void set_width_height(uint32_t width, uint32_t height);
 
@@ -44,6 +55,7 @@ namespace narechi::gui
         window(const window_properties& props);
 
         virtual void set_width_height_impl(uint32_t width, uint32_t height) = 0;
+
         window_properties props;
 
         bool has_rendered = false;
