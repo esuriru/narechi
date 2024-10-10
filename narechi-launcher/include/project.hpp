806850ea2df363ebc0fbaeaae0c23513f562dfa2
Christian <logicismmc@gmail.com>
2024-10-10 09:52:10 +0800

    Add basic serialize code to `project`

--- narechi-launcher/include/project.hpp
+++ narechi-launcher/include/project.hpp
@@ -1,14 +1,24 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
 
 namespace narechi::editor
 {
+    struct project_properties
+    {
+        std::string name;
+    };
+
     class project
     {
+    public:
+        static void serialize(
+            const std::filesystem::path& output_path, const project& project);
+        
+        project(const project_properties& props);
 
     private:
         std::string name;
-
     };
 }
