a41ec79c7ce6acfc9ed9a32bec3790b0a0418159
Christian <logicismmc@gmail.com>
2024-10-24 10:40:27 +0800

    Add name to `scene`

--- narechi/include/scene/scene.hpp
+++ narechi/include/scene/scene.hpp
@@ -13,13 +13,18 @@ namespace narechi::scene
     class NRC_API scene
     {
     public:
-        scene(const std::filesystem::path& path);
+        scene(const std::string& name, const std::filesystem::path& path);
         ~scene();
 
         void awake();
         void update(float delta_time);
 
+        void add_entity();
+
+        void save();
+
     private:
+        std::string name;
         flecs::world world;
         uptr<asset::scene_asset> asset;
 
--- narechi/src/scene/scene.cpp
+++ narechi/src/scene/scene.cpp
@@ -4,10 +4,12 @@
 
 namespace narechi::scene
 {
-    scene::scene(const std::filesystem::path& path)
-        : world()
+    scene::scene(const std::string& name, const std::filesystem::path& path)
+        : name(name)
+        , world()
         , asset(make_uptr<asset::scene_asset>(path, *this))
     {
+        asset->write();
     }
 
     scene::~scene()
@@ -24,5 +26,15 @@ namespace narechi::scene
     void scene::update(float delta_time)
     {
         world.progress(delta_time);
+    }
+
+    void scene::add_entity()
+    {
+        world.entity();
+    }
+
+    void scene::save()
+    {
+        asset->write();
     }
 }
