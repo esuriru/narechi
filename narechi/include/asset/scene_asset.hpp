7a9e4e05f69d222cc4bce6233e0c3310f6138bab
Christian <logicismmc@gmail.com>
2024-10-25 10:47:18 +0800

    Modify `scene_asset` to YAML and add ownership transfer method

--- narechi/include/asset/scene_asset.hpp
+++ narechi/include/asset/scene_asset.hpp
@@ -3,20 +3,28 @@
 #include "asset/asset.hpp"
 #include "core/core.hpp"
 
-NRC_FORWARD_DECL_CLASS(narechi::scene, scene);
+#include "scene/scene.hpp"
 
 namespace narechi::asset
 {
-    class scene_asset : public asset
+    class NRC_API scene_asset : public asset
     {
     public:
-        scene_asset(const std::filesystem::path& path, scene::scene& scene);
+        scene_asset(const std::filesystem::path& path,
+            scene::scene::scene_data* scene_data);
         ~scene_asset();
 
+        static uptr<scene_asset> load_data(const std::filesystem::path& path);
+
+        const scene::scene::scene_data& get_scene_data() const;
+
+        void to_owned(uptr<scene::scene::scene_data>& scene_data);
+
     private:
+        YAML::Node node;
+        scene::scene::scene_data* scene_data;
+
         void serialize() override;
         void deserialize() override;
-
-        scene::scene& scene;
     };
 }
--- narechi/src/asset/scene_asset.cpp
+++ narechi/src/asset/scene_asset.cpp
@@ -1,25 +1,90 @@
 #include "asset/scene_asset.hpp"
 
-#include "scene/scene.hpp"
+#include "yaml-cpp/yaml.h"
+
+namespace YAML
+{
+    using narechi::scene::scene;
+
+    template<>
+    struct convert<scene::scene_data>
+    {
+        static bool decode(const Node& node, scene::scene_data& data)
+        {
+            if (!node.IsMap())
+            {
+                return false;
+            }
+
+            data.name = node["Name"].as<std::string>();
+            data.world.from_json(node["World Data"].as<std::string>().c_str());
+            return true;
+        }
+    };
+}
 
 namespace narechi::asset
 {
     scene_asset::scene_asset(
-        const std::filesystem::path& path, scene::scene& scene)
+        const std::filesystem::path& path, scene::scene::scene_data* scene_data)
         : asset(path)
-        , scene(scene)
+        , scene_data(scene_data)
+        , node()
     {
     }
 
-    scene_asset::~scene_asset() = default;
+    scene_asset::~scene_asset()
+    {
+        if (is_owning)
+        {
+            delete scene_data;
+        }
+    }
+
+    uptr<scene_asset> scene_asset::load_data(const std::filesystem::path& path)
+    {
+        uptr<scene_asset> asset
+            = make_uptr<scene_asset>(path, new scene::scene::scene_data());
+        asset->is_owning = true;
+        asset->load();
+        return std::move(asset);
+    }
+
+    const scene::scene::scene_data& scene_asset::get_scene_data() const
+    {
+        return *scene_data;
+    }
+
+    void scene_asset::to_owned(uptr<scene::scene::scene_data>& scene_data)
+    {
+        is_owning = false;
+        scene_data.reset(this->scene_data);
+    }
 
     void scene_asset::serialize()
     {
-        data = scene.world.to_json();
+        if (node.IsNull())
+        {
+            node = YAML::Node(YAML::NodeType::Map);
+            node.force_insert("Name", scene_data->name);
+            node.force_insert(
+                "World Data", scene_data->world.to_json().c_str());
+        }
+        else
+        {
+            node["Name"] = scene_data->name;
+            node["World Data"] = scene_data->world.to_json().c_str();
+        }
+
+        YAML::Emitter emitter;
+        emitter << node;
+
+        data = emitter.c_str();
     }
 
     void scene_asset::deserialize()
     {
-        scene.world.from_json(data.c_str());
+        node = YAML::Load(data);
+        *scene_data = node.as<scene::scene::scene_data>();
     }
 }
