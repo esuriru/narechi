c953dfb62908b3b4ca0ef9ab73f2e2a476cb7105
Christian <logicismmc@gmail.com>
2024-10-25 10:46:22 +0800

    Create `scene_data` DTO and create factories for `scene`

--- narechi/include/scene/scene.hpp
+++ narechi/include/scene/scene.hpp
@@ -13,8 +13,20 @@ namespace narechi::scene
     class NRC_API scene
     {
     public:
-        scene(const std::string& name, const std::filesystem::path& path);
+        struct scene_data
+        {
+            std::string name;
+            flecs::world world;
+        };
+
+        scene();
+        scene(const std::filesystem::path& path);
         ~scene();
+
+        static sptr<scene> create(
+            const std::filesystem::path& path, const std::string& name);
+        static sptr<scene> load(const std::filesystem::path& path);
+        static sptr<scene> load(uptr<asset::scene_asset> scene_asset);
 
         void awake();
         void update(float delta_time);
@@ -23,11 +35,12 @@ namespace narechi::scene
 
         void save();
 
-    private:
-        std::string name;
-        flecs::world world;
-        uptr<asset::scene_asset> asset;
+        std::string get_name() const;
 
+    private:
         friend class asset::scene_asset;
+
+        uptr<scene_data> data;
+        uptr<asset::scene_asset> asset;
     };
 }
--- narechi/src/scene/scene.cpp
+++ narechi/src/scene/scene.cpp
@@ -1,13 +1,19 @@
 #include "scene/scene.hpp"
 
 #include "asset/scene_asset.hpp"
+#include "flecs/addons/cpp/world.hpp"
 
 namespace narechi::scene
 {
-    scene::scene(const std::string& name, const std::filesystem::path& path)
-        : name(name)
-        , world()
-        , asset(make_uptr<asset::scene_asset>(path, *this))
+    scene::scene()
+        : data(nullptr)
+        , asset(nullptr)
+    {
+    }
+
+    scene::scene(const std::filesystem::path& path)
+        : data(make_uptr<scene::scene_data>())
+        , asset(make_uptr<asset::scene_asset>(path, data.get()))
     {
         asset->write();
     }
@@ -15,27 +21,56 @@ namespace narechi::scene
     scene::~scene()
     {
         asset.reset();
-        world.release();
+        data->world.release();
+    }
+
+    sptr<scene> scene::create(
+        const std::filesystem::path& path, const std::string& name)
+    {
+        sptr<scene> new_scene = make_sptr<scene>(path);
+        new_scene->data->name = name;
+        new_scene->save();
+        return new_scene;
+    }
+
+    sptr<scene> scene::load(const std::filesystem::path& path)
+    {
+        sptr<scene> existing_scene = make_sptr<scene>(path);
+        existing_scene->asset->load();
+        return existing_scene;
+    }
+
+    sptr<scene> scene::load(uptr<asset::scene_asset> scene_asset)
+    {
+        sptr<scene> existing_scene = make_sptr<scene>();
+        existing_scene->asset = std::move(scene_asset);
+        existing_scene->asset->to_owned(existing_scene->data);
+        return existing_scene;
     }
 
     void scene::awake()
     {
-        flecs::entity test_entity = world.entity();
+        flecs::entity test_entity = data->world.entity();
         test_entity.add(flecs::Disabled);
     }
 
     void scene::update(float delta_time)
     {
-        world.progress(delta_time);
+        data->world.progress(delta_time);
     }
 
     void scene::add_entity()
     {
-        world.entity();
+        data->world.entity();
     }
 
     void scene::save()
     {
         asset->write();
+    }
+
+    std::string scene::get_name() const
+    {
+        return data->name;
     }
 }
