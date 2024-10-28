56cf5783ddfb7c7adaa1cf2d658c998a579d4707
Christian <97773844+esuriru@users.noreply.github.com>
2024-10-29 02:08:23 +0800

    Inherit from `image_button_element` in `imgui_image_button_element`

--- narechi/include/imgui/imgui_image_button_element.hpp
+++ narechi/include/imgui/imgui_image_button_element.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
+#include "gui/image_button_element.hpp"
 #include "imgui/imgui_button_element.hpp"
+
+struct ImVec2;
 
 namespace narechi
 {
-    class NRC_API imgui_image_button_element : public imgui_button_element
+    class NRC_API imgui_image_button_element
+        : public imgui_button_element,
+          public virtual gui::image_button_element
     {
     public:
-        imgui_image_button_element(const gui::button_element_properties& props);
+        imgui_image_button_element(const gui::button_element_properties& props,
+            sptr<rendering::texture2d> texture);
 
         bool imgui_button_call() override;
+    private:
+        const ImVec2 get_image_size();
     };
 }
--- narechi/src/imgui/imgui_image_button_element.cpp
+++ narechi/src/imgui/imgui_image_button_element.cpp
@@ -1,22 +1,34 @@
 #include "imgui/imgui_image_button_element.hpp"
 
-#include "core/logger.hpp"
+#include "gui/button_element.hpp"
+#include "gui/image_button_element.hpp"
 #include "imgui.h"
 
 namespace narechi
 {
     imgui_image_button_element::imgui_image_button_element(
-        const gui::button_element_properties& props)
-        : imgui_button_element(props)
+        const gui::button_element_properties& props,
+        sptr<rendering::texture2d> texture)
+        : gui::image_button_element(texture)
+        , gui::button_element(props)
     {
     }
 
     bool imgui_image_button_element::imgui_button_call()
     {
-        return ImGui::ImageButton(props.label.c_str(),
-            (ImTextureID)(intptr_t)props.texture->get_gfx_ctx_id(),
-            ImVec2(props.texture->get_width(), props.texture->get_height()),
+        return ImGui::ImageButton(uid.c_str(),
+            reinterpret_cast<ImTextureID>(
+                static_cast<intptr_t>(texture->get_gfx_ctx_id())),
+            get_image_size(),
             ImVec2(0.0f, 0.0f),
             ImVec2(1.0f, 1.0f));
+    }
+
+    const ImVec2 imgui_image_button_element::get_image_size()
+    {
+        return props.width != props.null_size
+                && props.height != props.null_size ?
+            ImVec2(props.width, props.height) :
+            ImVec2(texture->get_width(), texture->get_height());
     }
 }
