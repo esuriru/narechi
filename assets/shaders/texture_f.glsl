a94fd55f4c7342cfc44971f4c656fca505950187
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-06 00:56:34 +0800

    Create base opengl shaders

---
+++ assets/shaders/texture_f.glsl
@@ -0,0 +1,6 @@
+#version 460
+
+void main()
+{
+    return vec4(1.0f);
+}
---
+++ assets/shaders/texture_v.glsl
@@ -0,0 +1,8 @@
+#version 460
+
+layout(location = 0) in vec3 vertex_pos;
+
+void main()
+{
+    gl_Position = vec4(1.0f);
+}
