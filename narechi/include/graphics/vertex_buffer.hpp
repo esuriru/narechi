94d21d497b73202a20c2eefc94d86d154402f5dc
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-05 23:01:18 +0800

    Create `buffer_layout` struct and add missing `create()` impl

--- narechi/include/graphics/vertex_buffer.hpp
+++ narechi/include/graphics/vertex_buffer.hpp
@@ -1,19 +1,33 @@
 #pragma once
 
 #include "core/core.hpp"
+
 #include "graphics/handle.hpp"
+#include "graphics/buffer_element.hpp"
+
+#include <vector>
 
 namespace narechi::graphics
 {
     class vertex_buffer : public handle
     {
     public:
+        struct buffer_layout
+        {
+            std::vector<graphics::buffer_element> elements;
+        };
+
         virtual ~vertex_buffer() = default;
 
         virtual void bind() = 0;
         virtual void unbind() = 0;
 
+        virtual const buffer_layout& get_layout() const;
+
         static sptr<vertex_buffer> create(
             const float* vertices, const uint32_t size);
+
+    private:
+        buffer_layout layout;
     };
 }
--- narechi/src/graphics/vertex_buffer.cpp
+++ narechi/src/graphics/vertex_buffer.cpp
@@ -1,10 +1,33 @@
 #include "graphics/vertex_buffer.hpp"
+#include "graphics/renderer_api.hpp"
+
+#ifdef NRC_RENDERER_API_OPENGL
+    #include "platform/opengl/opengl_vertex_buffer.hpp"
+#endif
 
 namespace narechi::graphics
 {
     sptr<vertex_buffer> vertex_buffer::create(
         const float* vertices, const uint32_t size)
     {
-        
+        switch (renderer_api::get_api())
+        {
+        case renderer_api::api::opengl:
+#ifdef NRC_RENDERER_API_OPENGL
+            return make_sptr<opengl_vertex_buffer>(vertices, size);
+#else
+            NRC_CORE_FATAL("OpenGL is not compiled, but selected");
+#endif
+        default:
+            break;
+        }
+
+        NRC_CORE_ERROR("No vertex array available for selected Renderer API");
+        return nullptr;
+    }
+
+    const vertex_buffer::buffer_layout& vertex_buffer::get_layout() const
+    {
+        return this->layout;
     }
 }
