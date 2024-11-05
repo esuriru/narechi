8348a732b52f264516663de32602aa6cf5a3e9fe
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-06 00:46:45 +0800

    Add macro for embedding `std::string`

--- narechi/include/asset/embed.hpp
+++ narechi/include/asset/embed.hpp
@@ -6,3 +6,9 @@
         extern const uint8_t name[];         \
         extern const uint32_t name##_length; \
     }
+
+#define NRC_DECL_EMBED_STD_STRING(name) \
+    namespace narechi::embed            \
+    {                                   \
+        extern const std::string name;  \
+    }
