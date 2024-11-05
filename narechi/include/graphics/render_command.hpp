32723704eb58fcc85cce83770026357034d76e77
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-06 00:06:10 +0800

    Add `draw_indexed` and move definitions to source file in `render_command`

--- narechi/include/graphics/render_command.hpp
+++ narechi/include/graphics/render_command.hpp
@@ -2,31 +2,22 @@
 
 #include "core/core.hpp"
 #include "graphics/renderer_api.hpp"
+#include "graphics/vertex_array.hpp"
 
 namespace narechi::graphics
 {
     class render_command final
     {
     public:
-        static void init()
-        {
-            renderer_api->init();
-        }
-
-        static void cleanup()
-        {
-            renderer_api->cleanup();
-        }
+        static void init();
+        static void cleanup();
 
         // TODO -  Color class
-        static void clear_color(const glm::vec4& color)
-        {
-            renderer_api->clear_color(color);
-        }
+        static void clear_color(const glm::vec4& color);
 
-        static void draw()
-        {
-        }
+        static void draw_indexed(
+            std::shared_ptr<graphics::vertex_array> vertex_array,
+            uint32_t index_count = 0);
 
     private:
         static uptr<renderer_api> renderer_api;
--- narechi/src/graphics/render_command.cpp
+++ narechi/src/graphics/render_command.cpp
@@ -1,6 +1,29 @@
 #include "graphics/render_command.hpp"
+#include "graphics/renderer_api.hpp"
 
 namespace narechi::graphics
 {
     uptr<renderer_api> render_command::renderer_api = renderer_api::create();
+
+    void render_command::init()
+    {
+        renderer_api->init();
+    }
+
+    void render_command::cleanup()
+    {
+        renderer_api->cleanup();
+    }
+
+    void render_command::clear_color(const glm::vec4& color)
+    {
+        renderer_api->clear_color(color);
+    }
+
+    void render_command::draw_indexed(
+        std::shared_ptr<graphics::vertex_array> vertex_array,
+        uint32_t index_count)
+    {
+        renderer_api->draw_indexed(vertex_array, index_count);
+    }
 }
