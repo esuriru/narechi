59e81e8f19f5e1a12259f32e3335711a6853be20
Christian <logicismmc@gmail.com>
2024-11-05 09:39:16 +0800

    Add missing code

--- narechi/src/graphics/vertex_array.cpp
+++ narechi/src/graphics/vertex_array.cpp
@@ -1,26 +1,29 @@
 #include "graphics/vertex_array.hpp"
 
 #include "core/logger.hpp"
+#include "graphics/renderer_api.hpp"
 
 #ifdef NRC_RENDERER_API_OPENGL
-// #include "platform/opengl/opengl_vertex_array.hpp"
+    #include "platform/opengl/opengl_vertex_array.hpp"
 #endif
 
 namespace narechi::graphics
 {
     sptr<vertex_array> vertex_array::create()
     {
-    case renderer_api::api::opengl:
+        switch (renderer_api::get_api())
+        {
+        case renderer_api::api::opengl:
 #ifdef NRC_RENDERER_API_OPENGL
-        return make_sptr<opengl_vertex_array>();
+            return make_sptr<opengl_vertex_array>();
 #else
-        NRC_CORE_FATAL("OpenGL is not compiled, but selected");
+            NRC_CORE_FATAL("OpenGL is not compiled, but selected");
 #endif
-    default:
-        break;
-    }
+        default:
+            break;
+        }
 
-    NRC_CORE_ERROR("No vertex array available for selected Renderer API");
-    return nullptr;
-}
+        NRC_CORE_ERROR("No vertex array available for selected Renderer API");
+        return nullptr;
+    }
 }
