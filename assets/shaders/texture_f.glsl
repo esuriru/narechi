a701d14d843690450129a5b6363bb3afc8553c83
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-09 00:28:56 +0800

    Adjust shaders for texture rendering

--- assets/shaders/texture_f.glsl
+++ assets/shaders/texture_f.glsl
@@ -2,7 +2,11 @@
 
 layout(location = 0) out vec4 color;
 
+in vec2 frag_uv;
+
+uniform sampler2D tex;
+
 void main()
 {
-    color = vec4(1.0);
+    color = texture(tex, frag_uv);
 }
--- assets/shaders/texture_v.glsl
+++ assets/shaders/texture_v.glsl
@@ -1,10 +1,14 @@
 #version 460 core
 
 layout(location = 0) in vec3 vertex_pos;
+layout(location = 1) in vec2 vertex_uv;
+
+out vec2 frag_uv;
 
 uniform mat4 MVP;
 
 void main()
 {
+    frag_uv = vertex_uv;
     gl_Position = MVP * vec4(vertex_pos, 1.0);
 }
