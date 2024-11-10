d6f1f3773f939abe208b5704f75875399bd19dce
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-10 23:36:28 +0800

    Create prototype `database` to store assets to be queried at runtime

---
+++ narechi/include/asset/database.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "core/core.hpp"
+#include "core/assert.hpp"
+
+#include <unordered_map>
+#include <string>
+
+namespace narechi::asset
+{
+    class database
+    {
+    public:
+        database() = default;
+
+        template<typename T>
+        void add_asset(sptr<T> asset)
+        {
+            NRC_ASSERT(
+                !asset->get_guid().empty(), "Asset GUID cannot be empty");
+            asset_map[asset->get_guid()] = asset;
+        }
+
+        template<typename T>
+        sptr<T> get_asset(const std::string& guid)
+        {
+            const auto& it = asset_map.find(guid);
+            return it != asset_map.end() ?
+                std::static_pointer_cast<T>(it->second) :
+                nullptr;
+        }
+
+    private:
+        std::unordered_map<std::string, sptr<void>> asset_map;
+    };
+}
