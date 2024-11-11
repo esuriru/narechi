b9b22c48c049f5e1fa3fe480c820c8b73c05192f
Christian <logicismmc@gmail.com>
2024-11-11 10:59:31 +0800

    Change `component::sprite` to use GUID instead and serialize it

--- narechi/include/scene/component.hpp
+++ narechi/include/scene/component.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "asset/sprite_asset.hpp"
+#include "core/app.hpp"
 #include "core/logger.hpp"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/glm.hpp"
@@ -27,18 +29,34 @@ namespace narechi::scene
 
         struct sprite
         {
-            sptr<graphics::texture2d> texture;
+            // sptr<graphics::texture2d> texture;
             std::string texture_asset_guid;
         };
 
         component(flecs::world& world)
         {
             // world.module<component>();
+
+            // From flecs examples
+            world.component<std::string>()
+                .opaque(flecs::String)
+                .serialize(
+                    [](const flecs::serializer* s, const std::string* data)
+                    {
+                        const char* str = data->c_str();
+                        return s->value(flecs::String, &str);
+                    })
+                .assign_string(
+                    [](std::string* data, const char* value)
+                    {
+                        *data = value;
+                    });
             world.component<glm::vec2>()
                 .member("x", &glm::vec2::x)
                 .member("y", &glm::vec2::y);
+
             world.component<position>().member<glm::vec2>("value");
-            world.component<sprite>();
+            world.component<sprite>().member<std::string>("texture_asset_guid");
 
             world.component<scene_camera>();
             world.component<meta>();
@@ -52,8 +70,16 @@ namespace narechi::scene
                           [](const component::position& pos,
                               const component::sprite& sprite)
                           {
-                              graphics::render2d::submit_quad(
-                                  pos.value, { 20, 20 }, sprite.texture);
+                              auto& database = app::get().get_asset_database();
+                              auto sprite_asset
+                                  = database.get_asset<asset::sprite_asset>(
+                                      sprite.texture_asset_guid);
+                              if (sprite_asset)
+                              {
+                                  graphics::render2d::submit_quad(pos.value,
+                                      { 20, 20 },
+                                      sprite_asset->get_texture());
+                              }
                           });
 
             flecs::system camera_update_view_system
