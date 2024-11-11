99d0cfc5170c2463967c1326eab86373dfd07d9b
Christian <logicismmc@gmail.com>
2024-11-11 10:58:28 +0800

    Add impl for load and create in `sprite_asset`

--- narechi/include/asset/sprite_asset.hpp
+++ narechi/include/asset/sprite_asset.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "graphics/texture2d.hpp"
 #include "asset/asset.hpp"
 #include "yaml-cpp/yaml.h"
 
@@ -7,14 +8,19 @@ NRC_FORWARD_DECL_CLASS(narechi::graphics, sprite)
 
 namespace narechi::asset
 {
-    class sprite_asset : public asset
+    class NRC_API sprite_asset : public asset
     {
     public:
         sprite_asset(
             const std::filesystem::path& path, graphics::sprite* owner);
 
+        static sptr<sprite_asset> create(const std::filesystem::path& path);
+        static sptr<sprite_asset> load_data(const std::filesystem::path& path);
+
+        sptr<graphics::texture2d> get_texture() const;
+
     private:
-        // YAML::Node node;
+        sptr<graphics::texture2d> texture;
         graphics::sprite* owner;
 
         void serialize() override;
--- narechi/src/asset/sprite_asset.cpp
+++ narechi/src/asset/sprite_asset.cpp
@@ -6,9 +6,36 @@ namespace narechi::asset
 {
     sprite_asset::sprite_asset(
         const std::filesystem::path& path, graphics::sprite* owner)
-        : asset(path)
+        : asset(path.parent_path() / (path.stem().string() + ".nrcsprite"))
         , owner(owner)
     {
+        if (!owner)
+        {
+            is_owning = true;
+        }
+        texture = graphics::texture2d::load(path, {});
+    }
+
+    sptr<graphics::texture2d> sprite_asset::get_texture() const
+    {
+        return texture;
+    }
+
+    sptr<sprite_asset> sprite_asset::create(const std::filesystem::path& path)
+    {
+        sptr<sprite_asset> new_asset = make_sptr<sprite_asset>(path, nullptr);
+        new_asset->write();
+        return new_asset;
+    }
+
+    sptr<sprite_asset> sprite_asset::load_data(
+        const std::filesystem::path& path)
+    {
+        sptr<sprite_asset> existing_asset
+            = make_sptr<sprite_asset>(path, nullptr);
+        existing_asset->is_owning = true;
+        existing_asset->load();
+        return existing_asset;
     }
 
     void sprite_asset::serialize()
