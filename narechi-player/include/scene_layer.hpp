22b7c1a97bc5e06b7fcfc5638ac001f66d400b99
Christian <97773844+esuriru@users.noreply.github.com>
2024-11-26 17:40:57 +0800

    Implement `scene_layer` as runtime layer for a scene

--- narechi-player/include/scene_layer.hpp
+++ narechi-player/include/scene_layer.hpp
@@ -10,5 +10,12 @@ namespace narechi::player
         scene_layer();
 
         void on_attach() override;
+        void on_update(float delta_time) override;
+        void on_event(event& event) override;
+
+    private:
+        sptr<scene::scene> current_scene;
+
+        void invalidate_proj_matrix(float width, float height);
     };
 }
--- narechi-player/src/scene_layer.cpp
+++ narechi-player/src/scene_layer.cpp
@@ -1,4 +1,7 @@
 #include "scene_layer.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+
+#include <filesystem>
 
 namespace narechi::player
 {
@@ -9,6 +12,74 @@ namespace narechi::player
 
     void scene_layer::on_attach()
     {
-        // Attempt to find scene/project and load it
+        using namespace asset;
+
+        NRC_CORE_LOG("Attempting to load scene");
+        std::filesystem::path asset_dir
+            = std::filesystem::current_path() / "assets";
+        for (const auto& it : std::filesystem::directory_iterator(asset_dir))
+        {
+            // Load scene
+            if (it.is_regular_file()
+                && it.path().extension() == extension<scene_asset>::value)
+            {
+                current_scene = scene::scene::load(it.path());
+                NRC_VERIFY(current_scene,
+                    "Could not load scene at path: ",
+                    it.path().string());
+                NRC_CORE_LOG("Scene loaded: ", current_scene->get_name());
+            }
+
+            // Load sprites
+            if (it.is_regular_file()
+                && it.path().extension() == extension<sprite_asset>::value)
+            {
+                std::filesystem::path image_path = it.path();
+                image_path.replace_extension(".png");
+
+                app::get().get_asset_database().add_asset(
+                    sprite_asset::load_data(image_path));
+                NRC_CORE_LOG("Image loaded: ", image_path.string());
+            }
+        }
+
+        if (!current_scene)
+        {
+            NRC_CORE_WARN("There is no scene in the directory");
+        }
+
+        auto& window = app::get().get_window();
+        invalidate_proj_matrix(window.get_width(), window.get_height());
+    }
+
+    void scene_layer::on_update(float delta_time)
+    {
+        if (current_scene)
+        {
+            current_scene->update(delta_time);
+        }
+    }
+
+    void scene_layer::on_event(event& event)
+    {
+        event_handler handler(event);
+
+        handler.handle<window_resize_event>(
+            [this](window_resize_event& event)
+            {
+                graphics::render_command::set_viewport(
+                    0, 0, event.get_width(), event.get_height());
+                invalidate_proj_matrix(event.get_width(), event.get_height());
+                return false;
+            });
+    }
+
+    void scene_layer::invalidate_proj_matrix(float width, float height)
+    {
+        float scale = 0.05f;
+        float p_width = (static_cast<float>(width) * scale) / 2;
+        float p_height = (static_cast<float>(height) * scale) / 2;
+        graphics::render2d::set_proj_matrix(
+            glm::ortho(-p_width, p_width, -p_height, p_height, -10.0f, 10.0f));
     }
 }
