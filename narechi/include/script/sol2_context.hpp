8b59dbaf8981c91c7c3f47a40cb38362761618b5
Christian <97773844+esuriru@users.noreply.github.com>
2025-01-12 23:11:35 +0800

    Create base `sol2_context`

---
+++ narechi/include/script/sol2_context.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "core/core.hpp"
+
+namespace narechi::script
+{
+    class NRC_API sol2_context
+    {
+    public:
+        sol2_context();
+    };
+}
---
+++ narechi/src/script/sol2_context.cpp
@@ -0,0 +1,10 @@
+#include "script/sol2_context.hpp"
+
+#include "sol/sol.hpp"
+
+namespace narechi::script
+{
+    sol2_context::sol2_context()
+    {
+    }
+}
