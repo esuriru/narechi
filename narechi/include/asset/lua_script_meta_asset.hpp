0cc9f0ca3ae7547d556c0a40fe8ff7f22b380331
Christian <97773844+esuriru@users.noreply.github.com>
2025-01-18 15:22:43 +0800

    Rename `lua_script_asset` to `lua_script_meta_asset` and own script in meta asset

---
+++ narechi/include/asset/lua_script_meta_asset.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "asset/asset.hpp"
+#include "script/lua_script.hpp"
+
+namespace narechi::asset
+{
+    class NRC_API lua_script_meta_asset : public asset
+    {
+    public:
+        lua_script_meta_asset(const std::filesystem::path& path);
+
+        static sptr<lua_script_meta_asset> load_existing_script(
+            const script::lua_script_deps& deps,
+            const std::filesystem::path& meta_path);
+        static sptr<lua_script_meta_asset> create(
+            const script::lua_script_deps& deps,
+            const std::filesystem::path& meta_path);
+
+        std::string get_code() const;
+        void compile();
+
+        sptr<script::lua_script> get_script() const;
+
+    private:
+        sptr<script::lua_script> script;
+
+        void serialize() override;
+        void deserialize() override;
+    };
+}
---
+++ narechi/src/asset/lua_script_meta_asset.cpp
@@ -0,0 +1,67 @@
+#include "asset/lua_script_meta_asset.hpp"
+#include "utils/file_utils.hpp"
+
+namespace narechi::asset
+{
+    lua_script_meta_asset::lua_script_meta_asset(
+        const std::filesystem::path& path)
+        : asset(path)
+    {
+    }
+
+    sptr<lua_script_meta_asset> lua_script_meta_asset::load_existing_script(
+        const script::lua_script_deps& deps,
+        const std::filesystem::path& meta_path)
+    {
+        sptr<lua_script_meta_asset> existing_asset
+            = make_sptr<lua_script_meta_asset>(meta_path);
+        existing_asset->asset::load();
+
+        std::filesystem::path code_path
+            = std::filesystem::path(meta_path).replace_extension(
+                script::lua_script::extension());
+        existing_asset->script = make_sptr<script::lua_script>(
+            deps, utils::file::open_file_as_string(code_path));
+        existing_asset->is_owning = true;
+        return existing_asset;
+    }
+
+    sptr<lua_script_meta_asset> lua_script_meta_asset::create(
+        const script::lua_script_deps& deps,
+        const std::filesystem::path& meta_path)
+    {
+        sptr<lua_script_meta_asset> new_asset
+            = make_sptr<lua_script_meta_asset>(meta_path);
+        std::filesystem::path code_path
+            = std::filesystem::path(meta_path).replace_extension(".lua");
+
+        new_asset->script = make_sptr<script::lua_script>(
+            deps, utils::file::open_file_as_string(code_path));
+        new_asset->is_owning = true;
+        new_asset->write();
+        return new_asset;
+    }
+
+    std::string lua_script_meta_asset::get_code() const
+    {
+        return this->data;
+    }
+
+    void lua_script_meta_asset::compile()
+    {
+        script->compile();
+    }
+
+    sptr<script::lua_script> lua_script_meta_asset::get_script() const
+    {
+        return script;
+    }
+
+    void lua_script_meta_asset::serialize()
+    {
+    }
+
+    void lua_script_meta_asset::deserialize()
+    {
+    }
+}
