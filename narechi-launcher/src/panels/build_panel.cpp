d90807d4c389914424b31670ca5735718fa4051b
Christian <97773844+esuriru@users.noreply.github.com>
2025-01-19 23:12:16 +0800

    Add build for non-embedded exectuable

--- narechi-launcher/src/panels/build_panel.cpp
+++ narechi-launcher/src/panels/build_panel.cpp
@@ -56,6 +56,43 @@ namespace narechi::editor
                 std::filesystem::path build_dir(
                     build_directory_input->get_text());
 
+#ifdef NRC_NO_EMBED
+                NRC_CORE_LOG("Copying player executable and dependencies");
+
+                std::filesystem::path editor_dir
+                    = std::filesystem::current_path();
+                std::vector<const char*> files_to_copy = {
+                    {
+                        "narechi-player.exe",
+                        "libflecs.dll",
+                        "libnarechi-engine.dll",
+                    },
+                };
+
+                auto copy_file = [editor_dir, build_dir](const char* file_name)
+                {
+                    std::filesystem::path path = editor_dir / file_name;
+                    NRC_ASSERT(std::filesystem::exists(path),
+                        "Path does not exist when building: ",
+                        path);
+
+                    // Have to manually delete because exception throws
+                    std::filesystem::path target_path = build_dir / file_name;
+                    if (std::filesystem::exists(target_path))
+                    {
+                        std::filesystem::remove(target_path);
+                    }
+
+                    std::filesystem::copy(path,
+                        target_path,
+                        std::filesystem::copy_options::overwrite_existing);
+                };
+
+                for (const auto& file_name : files_to_copy)
+                {
+                    copy_file(file_name);
+                }
+#else
                 NRC_CORE_LOG("Unpacking and creating player executable");
                 {
                     std::ofstream out_player_stream;
@@ -68,13 +105,14 @@ namespace narechi::editor
 
                 NRC_CORE_LOG("Unpacking and creating engine DLL");
                 {
-                    std::ofstream out_player_stream;
-                    out_player_stream.open(
+                    std::ofstream out_engine_stream;
+                    out_engine_stream.open(
                         build_dir / "libnarechi-engine.dll", std::ios::binary);
-                    out_player_stream.write(
+                    out_engine_stream.write(
                         reinterpret_cast<const char*>(embed::libnarechi_engine),
                         embed::libnarechi_engine_length);
                 }
+#endif
 
                 // TODO - Need to pack and unpack `libflecs.dll`
 
