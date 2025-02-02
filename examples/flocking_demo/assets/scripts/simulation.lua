0b5fd79181c5e75ff1416c910bed72c6dc26d473
Christian <97773844+esuriru@users.noreply.github.com>
2025-02-02 16:50:08 +0800

    Add init function for testing

--- examples/flocking_demo/assets/scripts/simulation.lua
+++ examples/flocking_demo/assets/scripts/simulation.lua
@@ -4,10 +4,18 @@ S_COEF = 0.2;
 A_COEF = 0.5;
 C_COEF = 0.5;
 
+function init_boids(narechi__scene__component__position, boid)
+    local pos = narechi__scene__component__position
+    pos:set_depth(2)
+    pos:set_vec2(vec2(math.random(), math.random()))
+    -- pos:set_vec2(vec2(2, 2))
+
+end
+
 function boid_movement(narechi__scene__component__position, boid)
     local pos = narechi__scene__component__position
     pos:set_depth(2)
 
-    boid:set_depth(2)
-    pos:set_vec2(pos:get_vec2() + boid:get_vec2())
+    -- boid:set_depth(2)
+    -- pos:set_vec2(pos:get_vec2() + boid:get_vec2())
 end
